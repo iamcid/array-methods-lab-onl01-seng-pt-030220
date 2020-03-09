31b415310259c2e432faeb3ca6f63b060897ed0f
Christopher Cid <iamcid17@gmail.com>
2020-03-09 04:24:49 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,16 +11,13 @@ def using_reverse(array)
 end
 
 def using_first(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.first
 end
 
 def using_last(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.last
 end
 
 def using_size(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
   array.size
 end
